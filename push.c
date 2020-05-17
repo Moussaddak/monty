68eb67b59596f20020c1d92763cb0b350c21952f
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-17 15:03:32 +0000

    monty byte code

--- push.c
+++ push.c
@@ -1,18 +1,28 @@
 #include "monty.h"
 /**
- *push - create new elements in a dlinked list
+ *push - push to stack
+ *@h: input
+ *@d: input
+ */
+void push(stack_t **h, data_t *d)
+{
+	add_dnodeint(h, d->arg);
+}
+/**
+ *add_dnodeint - prints number of elements in a dlinked list
  *@head: input of type stack_t
  *@n: input of type integer
  *Return: a new node of type stack_t
  */
-stack_t *push(stack_t **head, const int n)
+stack_t *add_dnodeint(stack_t **head, const int n)
 {
 	stack_t *new_node;
 
 	new_node = malloc(sizeof(stack_t));
 	if (!new_node)
 	{
-		return (NULL);
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
@@ -25,6 +35,5 @@ stack_t *push(stack_t **head, const int n)
 		}
 		*head = new_node;
 	}
-
 	return (new_node);
 }
