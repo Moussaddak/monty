205a92bd79f8910adcca29a1a2f2cc19b95e8ff4
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-17 14:59:28 +0000

    monty byte code

--- parse.c
+++ parse.c
@@ -3,19 +3,19 @@
  * parse - parses line of input
  * @buffer: line of input to parse
  * @ln: line number being evaluated
+ * @h: top of the stack
  */
-void parse(char *buffer, unsigned int ln)
+void parse(char *buffer, unsigned int ln, stack_t **h)
 {
 	char delims[] = " \t\n", *token;
 	data_t *data;
-	stack_t *head;
 
 	data = malloc(sizeof(data_t));
-		if (!data)
-		{
-			printf("Error: malloc failed\n");
-			exit(EXIT_FAILURE);
-		}
+	if (!data)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 	token = strtok(buffer, delims);
 	if (eval(token, 0))
 		data->op = strdup(token);
@@ -27,11 +27,11 @@ void parse(char *buffer, unsigned int ln)
 	token = strtok(NULL, delims);
 	if (eval(token, 1))
 		data->arg = atoi(token);
-	else
+	else if (!strcmp(data->op, "push"))
 	{
 		printf("L%d: usage: push integer\n", ln);
 		exit(EXIT_FAILURE);
 	}
-	run_op(data, &head);
+	run_op(data, h);
 	_free(data);
 }
