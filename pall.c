d0f3e53825d9e9423b75fc19b8010d62b7478af7
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-17 14:58:12 +0000

    monty byte code

--- pall.c
+++ pall.c
@@ -1,18 +1,23 @@
 #include "monty.h"
 /**
- *pall - prints all the elements of a stack_t list
- *@h: input of type stack_t
- *Return: the number of nodes
+ *pall- print function
+ *@h: input
+ *@d: input
  */
-size_t pall(const stack_t *h)
+void pall(stack_t **h, data_t *d __attribute__((unused)))
 {
-	size_t cpt_n = 0;
+	print_dlistint(*h);
+}
+/**
+ *print_dlistint - prints all the elements of a stack_t list
+ *@h: input of type stack_t
+ */
+void print_dlistint(stack_t *h)
+{
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		cpt_n++;
 	}
-	return (cpt_n);
 }
